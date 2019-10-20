e502862826d7986217bae5f593b7d9460bcad2b3
Renata Freitas <r.miriuk@gmail.com>
2019-10-20 10:25:18 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ end
 def print_first_directors_movie_titles
   i = 0
  while i < directors_database.length do
-   print directors_database[:"Stephen Spielberg"][i]
+   print directors_database["Stephen Spielberg"][i]
    i += 1
  end
 
