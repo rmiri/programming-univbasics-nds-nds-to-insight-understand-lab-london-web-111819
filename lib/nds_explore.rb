3913840ee18f8a17fe5e2c3139845f89618389dc
Renata Freitas <r.miriuk@gmail.com>
2019-10-20 10:56:35 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,6 +10,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  i = 0
+  while i < directors_database.length do
 print directors_database[0][:movies][i][:title]
+i += 1
+end
 
 end
