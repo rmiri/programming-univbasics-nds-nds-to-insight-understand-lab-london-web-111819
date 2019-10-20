5cecd085b41d05fa2065bfe7fa3dca350f3744f0
Renata Freitas <r.miriuk@gmail.com>
2019-10-20 10:30:15 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ end
 def print_first_directors_movie_titles
   i = 0
  while i < directors_database.length do
-   print directors_database[i][i]
+   print directors_database[i][:name]
    i += 1
  end
 
